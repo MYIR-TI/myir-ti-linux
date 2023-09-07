a2e9e1e2418da0bf905d00262f632dad8c08da36
duxy <568988005@qq.com>
2023-09-07 10:18:55 +0800

    FEAT: modify sii902x code

--- drivers/gpu/drm/bridge/sii902x.c
+++ drivers/gpu/drm/bridge/sii902x.c
@@ -244,12 +244,12 @@ static void sii902x_reset(struct sii902x *sii902x)
 	if (!sii902x->reset_gpio)
 		return;
 
-	gpiod_set_value(sii902x->reset_gpio, 1);
+	 gpiod_set_value_cansleep(sii902x->reset_gpio, 1);
 
 	/* The datasheet says treset-min = 100us. Make it 150us to be sure. */
 	usleep_range(150, 200);
 
-	gpiod_set_value(sii902x->reset_gpio, 0);
+	 gpiod_set_value_cansleep(sii902x->reset_gpio, 0);
 }
 
 static enum drm_connector_status sii902x_detect(struct sii902x *sii902x)
@@ -1228,7 +1228,6 @@ static int sii902x_probe(struct i2c_client *client,
 		regulator_bulk_disable(ARRAY_SIZE(sii902x->supplies),
 				       sii902x->supplies);
 	}
-
 	return ret;
 }
 
